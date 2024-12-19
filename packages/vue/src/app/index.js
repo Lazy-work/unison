54bcb790046b8b45d499d7f8e2ceced3be1a5b60
NGBAMA William <william.ngbama@gmail.com>
2024-12-20 00:04:12 +0100

    refactor(vue): refactor AppProvider to jsdoc

--- packages/vue/src/app/index.js
+++
@@ -1,7 +0,0 @@
-import { onUnmounted, $bridge } from '../index';
-export const AppProvider = $bridge((props) => {
-    onUnmounted(() => {
-        props.app.unmount();
-    });
-    return () => props.children;
-}, "AppProvider");

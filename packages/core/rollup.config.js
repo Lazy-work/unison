13aed543fa9c676aa59b1f4c1dca9d0864f0f47d
NGBAMA William <william.ngbama@gmail.com>
2025-01-01 03:52:55 +0100

    chore(core): add replace in rollup

--- packages/core/rollup.config.js
+++ packages/core/rollup.config.js
@@ -1,4 +1,5 @@
-import nodeResolve from "@rollup/plugin-node-resolve";
+import nodeResolve from '@rollup/plugin-node-resolve';
+import replace from '@rollup/plugin-replace';
 // import esbuild from "rollup-plugin-esbuild";
 // import path from "node:path";
 // import { fileURLToPath } from "node:url";
@@ -7,8 +8,28 @@ import nodeResolve from "@rollup/plugin-node-resolve";
 
 const plugins = [
   nodeResolve({
-    extensions: [".js", ".ts"],
+    extensions: ['.js', '.ts'],
     browser: true,
+  }),
+  replace({
+    __DEV__: `!!(process.env.NODE_ENV !== 'production')`,
+    __TEST__: `false`,
+    __BROWSER__: `false`,
+    __GLOBAL__: `false`,
+    __ESM_BUNDLER__: `false`,
+    __ESM_BROWSER__: `false`,
+    __CJS__: `false`,
+    __SSR__: `false`,
+    __COMMIT__: 'undefined',
+    __VERSION__: '3.5',
+    __COMPAT__: `false`,
+
+    // Feature flags
+    __FEATURE_OPTIONS_API__: `false`,
+    __FEATURE_PROD_DEVTOOLS__: `false`,
+    __FEATURE_SUSPENSE__: `false`,
+    __FEATURE_PROD_HYDRATION_MISMATCH_DETAILS__: `false`,
+    preventAssignment: true,
   }),
   // esbuild({
   //   tsconfig: path.resolve(__dirname, "tsconfig.json"),
@@ -19,20 +40,20 @@ const plugins = [
 
 export default [
   {
-    external: ["react"],
-    input: "src/index.js",
+    external: ['react'],
+    input: 'src/index.js',
     output: [
       {
-        file: "dist/briddge-core.esm-bundler.js",
-        format: "es",
-        sourcemap: true,
+        file: 'dist/briddge-core.esm-bundler.js',
+        format: 'es',
+        sourcemap: false,
       },
       {
-        file: "dist/briddge-core.cjs.js",
-        format: "cjs",
-        sourcemap: true,
-      }
+        file: 'dist/briddge-core.cjs.js',
+        format: 'cjs',
+        sourcemap: false,
+      },
     ],
     plugins,
-  }
+  },
 ];

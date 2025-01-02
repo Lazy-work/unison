f0482b482039272ed65d4b36defa1590229e65b0
NGBAMA William <william.ngbama@gmail.com>
2025-01-02 23:34:41 +0100

    fix(core): remove inline enum plugin

--- packages/core/rollup.config.js
+++ packages/core/rollup.config.js
@@ -2,19 +2,18 @@ import nodeResolve from '@rollup/plugin-node-resolve';
 import replace from '@rollup/plugin-replace';
 import alias from '@rollup/plugin-alias';
 import esbuild from 'rollup-plugin-esbuild';
-import InlineEnum from 'unplugin-inline-enum/rollup'
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 
 const __dirname = fileURLToPath(new URL('.', import.meta.url));
 
 const entries = {
-    "#vue-internals/reactivity": path.resolve(__dirname, "vue/packages/reactivity/src/"),
-    "#vue-internals/runtime-core": path.resolve(__dirname,"vue/packages/runtime-core/src/"),
-    "#vue-internals/shared": path.resolve(__dirname,"vue/packages/shared/src/"),
-    "#vue/reactivity": path.resolve(__dirname,"vue/packages/reactivity/src/index.ts"),
-    "#vue/runtime-core": path.resolve(__dirname,"vue/packages/runtime-core/src/index.ts"),
-    "#vue/shared": path.resolve(__dirname,"vue/packages/shared/src/index.ts")
+  '#vue-internals/reactivity': path.resolve(__dirname, 'vue/packages/reactivity/src/'),
+  '#vue-internals/runtime-core': path.resolve(__dirname, 'vue/packages/runtime-core/src/'),
+  '#vue-internals/shared': path.resolve(__dirname, 'vue/packages/shared/src/'),
+  '#vue/reactivity': path.resolve(__dirname, 'vue/packages/reactivity/src/index.ts'),
+  '#vue/runtime-core': path.resolve(__dirname, 'vue/packages/runtime-core/src/index.ts'),
+  '#vue/shared': path.resolve(__dirname, 'vue/packages/shared/src/index.ts'),
 };
 const plugins = [
   alias({
@@ -24,31 +23,30 @@ const plugins = [
     extensions: ['.js', '.ts'],
     browser: true,
   }),
-  // InlineEnum(),
   esbuild({
     tsconfig: path.resolve(__dirname, 'tsconfig.json'),
     sourceMap: true,
     target: 'esnext',
-  }),  
+  }),
   replace({
-    '__DEV__': `!!(process.env.NODE_ENV !== 'production')`,
-    __TEST__:`false`,
-    __BROWSER__:`false`,
-    __GLOBAL__:`false`,
-    __ESM_BUNDLER__:`false`,
-    __ESM_BROWSER__:`false`,
-    __CJS__:`false`,
-    __SSR__:`false`,
-    __COMMIT__: "undefined",
-    __VERSION__: "3.5",
-    __COMPAT__:`false`,
+    __DEV__: `!!(process.env.NODE_ENV !== 'production')`,
+    __TEST__: `false`,
+    __BROWSER__: `false`,
+    __GLOBAL__: `false`,
+    __ESM_BUNDLER__: `false`,
+    __ESM_BROWSER__: `false`,
+    __CJS__: `false`,
+    __SSR__: `false`,
+    __COMMIT__: 'undefined',
+    __VERSION__: '3.5',
+    __COMPAT__: `false`,
 
     // Feature flags
-    __FEATURE_OPTIONS_API__:`false`,
-    __FEATURE_PROD_DEVTOOLS__:`false`,
-    __FEATURE_SUSPENSE__:`false`,
-    __FEATURE_PROD_HYDRATION_MISMATCH_DETAILS__:`false`,
-    preventAssignment: true
+    __FEATURE_OPTIONS_API__: `false`,
+    __FEATURE_PROD_DEVTOOLS__: `false`,
+    __FEATURE_SUSPENSE__: `false`,
+    __FEATURE_PROD_HYDRATION_MISMATCH_DETAILS__: `false`,
+    preventAssignment: true,
   }),
 ];
 
